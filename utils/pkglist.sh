2c1e72480c5bc0562a309c2332e112608d22d3be
Watthanachai Dueanklang <bosskillerz@gmail.com>
2022-01-09 12:55:45 +0700

    modified:   PKGLIST.TXT 	modified:   utils/pkglist.sh

--- utils/pkglist.sh
+++ utils/pkglist.sh
@@ -19,13 +19,13 @@ done
 
 source ../0_common_head.sh
 
-echo "Distro name: <${PROJ}>" > ${FILE}
-echo "Created by: <${AUTHOR}>" >> ${FILE}
-echo "Reversion: <${REV}>" >> ${FILE}
-echo "Compilers: <${_languages}>" >> ${FILE}
-echo "Thread: <${X_THREAD}>" >> ${FILE}
-echo "Exception: <SEH for 64> <Draft-2 for 32>" >> ${FILE}
-echo "Libc: <${_default_msvcrt}>" >> ${FILE}
+echo "Distro name: ${PROJ}" > ${FILE}
+echo "Created by: ${AUTHOR}" >> ${FILE}
+echo "Reversion: ${REV}" >> ${FILE}
+echo "Compilers: ${_languages}" >> ${FILE}
+echo "Thread: ${X_THREAD}" >> ${FILE}
+echo "Exception: SEH for 64 and Draft-2 for 32" >> ${FILE}
+echo "Libc: ${_default_msvcrt}" >> ${FILE}
 echo "" >> ${FILE}
 echo "" >> ${FILE}
 echo "====================== Packages ======================" >> ${FILE}
