58f61582e3d85a7b69e90e590a4297966e198213
Watthanachai Dueanklang <bosskillerz@gmail.com>
2022-01-12 16:56:14 +0700

    modified:   32.BUILDALL.sh 	modified:   64.BUILDALL.sh

--- 32.BUILDALL.sh
+++ 32.BUILDALL.sh
@@ -4,4 +4,11 @@ DIR=$(dirname $0)
 
 cd ${DIR}
 
+cat 0_common_head.sh | grep "#_default_msvcrt=ucrt" || sed 's/_default_msvcrt=ucrt/#_default_msvcrt=ucrt/1' 0_common_head.sh -i
+
 sh BUILDALL32.S1.sh && sh BUILDALL32.S23.sh && sh BUILDALL32.Sfinal.sh && echo "===================== Success ====================="
+
+cd ${DIR}
+
+sh BUILDALL32.S1.sh && sh BUILDALL32.S23.sh && sh BUILDALL32.Sfinal.sh && echo "===================== Success ====================="
+
--- 64.BUILDALL.sh
+++ 64.BUILDALL.sh
@@ -4,4 +4,13 @@ DIR=$(dirname $0)
 
 cd ${DIR}
 
+cat 0_common_head.sh | grep "#_default_msvcrt=ucrt" || sed 's/_default_msvcrt=ucrt/#_default_msvcrt=ucrt/1' 0_common_head.sh -i
+
 sh BUILDALL64.S1.sh && sh BUILDALL64.S23.sh && sh BUILDALL64.Sfinal.sh && echo "===================== Success ====================="
+
+cd ${DIR}
+
+cat 0_common_head.sh | grep "_default_msvcrt=ucrt" || sed 's/#_default_msvcrt=ucrt/_default_msvcrt=ucrt/1' 0_common_head.sh -i
+
+sh BUILDALL64.S1.sh && sh BUILDALL64.S23.sh && sh BUILDALL64.Sfinal.sh && echo "===================== Success ====================="
+
